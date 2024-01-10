7047358832ca6b65398bb40f9b4295dfbcf5f62a
szupurgy <karcsirichi@gmail.com>
2024-01-10 22:16:00 +0100

    Update page.tsx

--- webx/app/page.tsx
+++ webx/app/page.tsx
@@ -5,7 +5,7 @@ import SajatDoboz from "./box";
 export default function Home() {
   const alma="Egy szép napon haza talaltam."
   return (
-    <main className="bg-black">
+    <main className="bg-gray-400">
       <SajatDoboz />
     </main>
   )
